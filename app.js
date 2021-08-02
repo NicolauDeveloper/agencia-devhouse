016bd893ae55a8f2e2f6cc55cfbf6ea412623f9e
Fabio Nicolauu <fpnicolau@gmail.com>
2021-08-02 14:22:59 -0300

    Alterações

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // criando um servidor http
 
 const express = require("express");
+const rotasDeProdutos = require("./routes/produtos");
+const rotas = require("./routes");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -21,6 +25,17 @@ app.get("/manutencao", (req, res) => {
 app.get("/blog", (req, res) => {
   res.sendFile(__dirname + "/views/blog.html");
 });
+
+app.get("/contato", (req, res) => {
+  res.sendFile(__dirname + "/views/contato.html");
+});
+
+app.post("/receber-contato", (req, res) => {
+  console.log(req.body);
+  res.send("Contato recebido por:" + req.body.nome);
+});
+app.use(rotasDeProdutos);
+app.use(rotas);
 
 app.listen(3000);
 
